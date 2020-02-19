e74f1f943df3d737bf702fcd58432ab0937604e4
Elijah wines <elijahwines84@yahoo.com>
2020-02-19 17:42:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,23 +1,9 @@
 def sort_array_asc(array)
   array.sort do |a,b|
-    if a == b
-      0
-    elsif a < b
-      -1
-    elsif a > b
-      1
-    end
+    a <=> b 
   end
 end
 
-def sort_array_desc(array)
-  array.sort do |a,b|
-    if a == b
-      0
-    elsif a < b
-      1
-    elsif a > b
-      -1
-    end
-  end
-end
+
+
+  
