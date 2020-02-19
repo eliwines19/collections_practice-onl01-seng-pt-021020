ea08aff28e48ff77e0d60f885e92a46f3ca058ca
Elijah wines <elijahwines84@yahoo.com>
2020-02-19 17:38:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,10 +1,10 @@
 def sort_array_asc(array)
   array.sort do |a,b|
-    if a == b 
+    if a == b
       0
-    elsif a < b 
+    elsif a < b
       -1
-    elsif a > b 
+    elsif a > b
       1
     end
 end      
